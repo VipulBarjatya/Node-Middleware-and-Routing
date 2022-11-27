793ddc95ad6848bafe01b1dcb70cf5cf45069fd8
Vipul <vipulbarjatya@gmail.com>
2022-11-27 18:34:37 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+const PORT = process.env.PORT || 3500;
+
+// CUSTOM MIDDLEWARE LOGGER
+app.use(logger);
+
+// Cross Origin resource sharing
+const whiteList = [
+  "https://www.yoursite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// BUILT IN MIDDLEWARE
+// in other words, form data:
+// "content-type": application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// BUILT IN MIDDLEWARE for JSON
+app.use(express.json());
+
+// SERVE STATIC FILES
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("^/$|/index(.html)?", (req, res) => {
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page(.html)?", (req, res) => {
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.redirect(301, "/new-page.html");
+});
+
+// Route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World");
+  }
+);
+
+// Chaining route handlers
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+const three = (req, res, next) => {
+  console.log("three");
+  res.send("Finished");
+};
+
+app.get("/chain(.html)?", [one, two, three]);
+
+app.get("/*", (req, res) => {
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
